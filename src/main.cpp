3b4657db1ba99da206da2c85bbebf7199c96fb04
kregerl <loucaskreger@gmail.com>
2022-02-12 11:15:20 -0500

    Added basic command line option for interpreting a single expression

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,48 @@
 
 // TODO: Convert double vectors in Vector and Set to Number vector.
 // TODO: Allow colon syntax for vectors and sets: [1:6] -> {1, 2, 3, 4, 5, 6} | [:2] -> {0, 1, 2}
-// TODO: Allow |x| syntax for absolute value, also add `abs(x)`
 
 
 //std::unordered_map<std::string, NumberNode *> globalVariables;
 
-int main() {
-    std::string expression;
-    auto interpreter = Interpreter();
-    while (expression != "exit") {
-        std::cout << "Mash > ";
-        getline(std::cin, expression);
-        if (expression == "exit") {
-            break;
-        } else {
-            try {
-                auto lexer = Lexer(expression);
-                auto tokens = lexer.tokenize();
-                auto parser = Parser(tokens);
-                AbstractNode *n = parser.parse();
-                std::cout << Evaluator::getValue(n) << std::endl;
-            } catch (EvaluatorException &e) {
-                std::cout << e.what() << std::endl;
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::string expression;
+        auto interpreter = Interpreter();
+        while (expression != "exit") {
+            std::cout << "Mash > ";
+            getline(std::cin, expression);
+            if (expression == "exit") {
+                break;
+            } else {
+                try {
+                    auto lexer = Lexer(expression);
+                    auto tokens = lexer.tokenize();
+                    auto parser = Parser(tokens);
+                    AbstractNode *n = parser.parse();
+                    std::cout << Evaluator::getValue(n) << std::endl;
+                } catch (EvaluatorException &e) {
+                    std::cout << e.what() << std::endl;
+                }
+            }
+        }
+    } else {
+        if (argc == 3) {
+            std::string flag = argv[1];
+            if (flag == "-i") {
+                try {
+                    auto lexer = Lexer(argv[2]);
+                    auto tokens = lexer.tokenize();
+                    auto parser = Parser(tokens);
+                    AbstractNode *n = parser.parse();
+                    std::cout << Evaluator::getValue(n) << std::endl;
+                } catch (EvaluatorException &e) {
+                    std::cout << e.what() << std::endl;
+                }
             }
         }
     }
+
 }
 
 
